e4bec058fa9e0f16d3a9a23d7f355ac1a8f4c645
mohammedAteff <mohamedatefabdelmonam@gmail.com>
2021-05-27 18:54:08 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let input = document.getElementById('taskdata');
+let addbtn = document.getElementById('addbtn');
+let notask = document.getElementById('notask');
+let tasks = document.getElementById('tasks');
+
+let showNoTasks = () => {
+    if (tasks.childNodes.length == 0){
+        notask.classList.remove('none')
+    }
+}
+
+let addTask = () => {
+    // notask.classList.toggle('none')
+    let task = input.value;
+    if (task.length == 0 || task.length < 2 || task.length > 10) {
+        alert("you must enter a valid data")
+        notask.classList.remove('none');
+        
+    } else {
+        notask.classList.add('none');
+        tasks.innerHTML += `<div class ='task alert alert-light'> ${task}
+        <button style="width: 30px;" class='delete float-right  btn btn-danger'> X </button>
+        <input  type="checkbox" class=' float-left'>
+        
+        </div>`;
+
+
+    }
+
+    tasks.addEventListener('click' ,function(ev){
+        if(ev.target.classList.contains('task')){
+            ev.target.classList.add('checked')
+        }
+
+    })
+
+    input.value = "";
+
+}
+addbtn.addEventListener('click', addTask);
+
+let deletetask = (e) => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+<<<<<<< HEAD
+        showNoTasks();
+=======
+        e.target.childNodes.toggle(showNoTasks);
+>>>>>>> 7affcaf5c6a282ff74d385dcb6e7e6b502969276
+    }
+
+}
+
+document.addEventListener('click', deletetask);
